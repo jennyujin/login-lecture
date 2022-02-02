b216b8d5a36cf3f53b03c7e3e04aaea3b31b319f
jennyujin <eppunijyj@gmail.com>
2022-02-02 22:53:14 +0900

    소스코드 src 폴더로 분류

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // Routing
-const home = require("./routes/home");
+const home = require("../routes/home");
 
 // App Setting
 app.set("views", "./views");
