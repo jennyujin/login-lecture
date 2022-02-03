c6f6f241ada9f97d9101b01de59f2e2a15d6313c
jennyujin <eppunijyj@gmail.com>
2022-02-04 01:42:01 +0900

    implement user model

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/Users");
+
 const output = {
     home: (req, res) => {
         res.render("home/index");
@@ -10,25 +11,10 @@ const output = {
     },
 };
 
-
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "login failed";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
