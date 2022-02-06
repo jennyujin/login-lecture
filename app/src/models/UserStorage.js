eea8f36722eb80ff67f39bc56d7b39aa38207315
jennyujin <eppunijyj@gmail.com>
2022-02-06 22:35:33 +0900

    implement login function via fs module

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,22 @@
 "use strict";
 
-class UserStorage {
-    static #users = {
-        id: ["eppunijyj", "jenny", "jang"],
-        psword: ["123", "1234", "12345"],
-        name: ["A", "B", "C"],
-    };
+const fs = require("fs").promises;
 
+class UserStorage {
+    static #getUserInfo() {
+        const users = JSON.parse(data);
+                const idx = users.id.indexOf(id);
+                const usersKeys = Object.keys(users); // => [id, psword, name]
+                const userInfo = usersKeys.reduce((newUser, info) => {
+                    newUser[info] = users[info][idx];
+                    return newUser;
+                }, {});
+
+                return userInfo;
+    }
+    
    static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -19,19 +27,16 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // => [id, psword, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
     }
 
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
