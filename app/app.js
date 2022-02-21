4326bd119eae90964f8407892007ac8016f23bb3
jennyujin <eppunijyj@gmail.com>
2022-02-22 00:54:16 +0900

    register and setup the log management module wiston

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
 
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 // Routing
 const home = require("./src/routes/home");
@@ -17,7 +20,9 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 방법 
 
